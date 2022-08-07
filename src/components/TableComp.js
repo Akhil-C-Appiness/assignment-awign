06226cfdf1a4cdc94749326395e32e09648f6e0d
Akhil-C-Appiness <akhil.c@appinessworld.com>
2022-08-07 13:31:14 +0530

    Added products data to table

---
+++ src/components/TableComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Table } from 'antd'
+const TableComp = ({columns,data}) => {
+  return (
+    <>
+    <Table columns={columns} dataSource={data}/>
+    </>
+  )
+}
+
+export default TableComp
