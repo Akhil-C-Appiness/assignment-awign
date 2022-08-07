1ba88f87f1acadbebdcd31eb6fb42180503c9904
Akhil-C-Appiness <akhil.c@appinessworld.com>
2022-08-07 12:13:04 +0530

    Inital & Search box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import "antd/dist/antd.css"
+import InputComp from './components/InputComp';
 
 function App() {
+  const [search,setSearch] =useState("")
+
+  const handleSearch = (val) => {
+    setSearch(val);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className='App'>
+    <div className="header">
+      <InputComp placeholder="Search" value={search}
+      handleSearch={handleSearch} />
     </div>
+    </section>
   );
 }
 
